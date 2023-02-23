c14bacd79691bbbca25e2946fedf2de3f62ab90a
joagBov <94766721+joagBov@users.noreply.github.com>
2023-02-23 16:40:59 -0500

    Added f5.cpp

---
+++ f5.cpp
@@ -0,0 +1,25 @@
+/*
+  By: Elijah Atta-Konadu
+  Filename: F5.cpp
+  Student ID: 872260
+  Description: A program that gets users input and finds the lowest number
+*/
+#include <iostream>
+using namespace std;
+
+int modifyArrayValues(int num[], const int kSize) {
+}
+int minArrayValue(int num[],const int kSize) {
+
+}
+int main() {
+	const int size = 10;
+	int num[size] = {};
+	int index = 0;
+	int min = -1;
+	modifyArrayValues(num,size);
+	index = minArrayValue(num, size);
+	cout << "The lowest v" << min << " at index " << index;
+}
+
+}
